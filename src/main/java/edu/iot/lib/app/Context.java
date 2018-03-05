228ddf4b86256f4257831df551adb573757cf92a
student <student@M120111>
2018-03-05 16:27:36 +0900

    version 1.1

---
+++ src/main/java/edu/iot/lib/app/Context.java
@@ -0,0 +1,24 @@
+package edu.iot.lib.app;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author student
+ * 모든 컴포넌트에서 공유해서 사용할 데이터 관리
+ */
+public class Context {
+	private static Map<String, Object> map = new HashMap<>();
+	
+	public static void setAttribute(String key, Object value) {
+		map.put(key, value);
+	}
+	
+	public static Object getAttribute(String key) {
+		return map.get(key);
+	}
+	
+	public static void remove(String key) {
+		map.remove(key);
+	}
+}
